186bdbbb653ea41e6830327959b587f1c2c42378
Gordon <gsitu322@gmail.com>
2018-03-06 22:56:29 -0800

    Added delete and simplified post route

--- routes/project.js
+++ routes/project.js
@@ -21,15 +21,7 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-
-    var project = new Project({
-        name: req.body.name,
-        description: req.body.description,
-        milestones: req.body.milestones,
-        technologies: req.body.technologies
-    });
-
-    project.save(function(err, project) {
+    Project.create(req.body, function(err, project) {
         if (err) {
             return res.status(500).json({
                 message: "Failed to save project",
@@ -40,6 +32,38 @@ router.post('/', function(req, res, next) {
         res.status(201).json({
             message: 'Successfully saved project',
             project: project
+        });
+    });
+});
+
+router.delete('/:id', function(req, res, next) {
+
+    Project.findById(req.params.id, function(err, projectFound) {
+        if (err) {
+            return res.status(500).json({
+                'title': 'Failed'
+            });
+        }
+
+        if (! projectFound) {
+            return res.status(500).json({
+                message: 'Project could not be found',
+                error: {message: 'No Project'}
+            });
+        }
+
+        projectFound.remove(function (err, result) {
+           if (err) {
+               return res.status(500).json({
+                   message: 'Failed to delete message',
+                   err: err
+               })
+           }
+
+           res.status(200).json({
+               message: "Successfully Delete Message",
+               result: result
+           });
         });
     });
 });
