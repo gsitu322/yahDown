b368a3f27180cad3761212ea73a3597e73c421a8
Gordon <gsitu322@gmail.com>
2018-03-10 03:02:45 -0800

    Added model on angular

---
+++ assets/app/components/project/project.model.ts
@@ -0,0 +1,8 @@
+export class Project {
+    constructor(
+        public name: string,
+        public description: string,
+        public milestones: string,
+        public technologies: string
+    ){}
+}
